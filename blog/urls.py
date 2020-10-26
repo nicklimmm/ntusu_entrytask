e99da4888c8a08f6f965d2ae8c513140dc723b39
nicklimmm <nickylim.p@gmail.com>
2020-10-26 23:15:08 +0800

    adding register

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,8 @@ from .views import Home, PostDetailed, AddPost, EditPost, DeletePost
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('post/<int:pk>/', PostDetailed.as_view(), name='postdetailed'),
+    path('post/<int:pk>', PostDetailed.as_view(), name='postdetailed'),
     path('addpost/', AddPost.as_view(), name='addpost'),
     path('post/<int:pk>/edit/', EditPost.as_view(), name='editpost'),
     path('post/<int:pk>/delete/', DeletePost.as_view(), name='deletepost')
-    # path('login/', loginuser, name='login'),
-    # path('register/', registeruser, name='register'),
 ]
