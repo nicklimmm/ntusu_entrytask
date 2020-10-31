6bd9898e1975035a6757353fcd5f2c5739da5c41
nicklimmm <nickylim.p@gmail.com>
2020-10-31 18:35:17 +0800

    Added user authentication, added categories (category dropdown, page showing posts with the same category, etc.), several bug fixes

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 
 from .forms import PostForm, EditForm
-from .models import Post
+from .models import Post, Category
 
 # def home(request):
 #     return render(request, 'home.html')
@@ -19,10 +19,35 @@ class Home(ListView):
     template_name = 'home.html'
     ordering = ['-date_posted']
 
+    
+    def get_context_data(self, *args,**kwargs):
+        categories_menu = Category.objects.all()
+        context = super(self.__class__, self).get_context_data(*args, **kwargs)
+        context['categories_menu'] = categories_menu
+        return context
+    
+
+
+def CategoryView(request, cats):
+    categories_menu = Category.objects.all()
+    category_posts = Post.objects.filter(category=cats.replace('-', ' '))
+    return render(request, 'category.html', {'cats':cats.title().replace('-', ' '), 'category_posts':category_posts, 'categories_menu': categories_menu})
+
+
+def CategoryList(request):
+    categories_menu_list = Category.objects.all()
+    return render(request, 'categorylist.html', {'categories_menu_list': categories_menu_list})
+
 
 class PostDetailed(DetailView):
     model = Post
     template_name = 'postdetailed.html'
+    
+    def get_context_data(self, *args,**kwargs):
+        categories_menu = Category.objects.all()
+        context = super(self.__class__, self).get_context_data(*args, **kwargs)
+        context['categories_menu'] = categories_menu
+        return context
 
 
 class AddPost(CreateView):
@@ -32,6 +57,24 @@ class AddPost(CreateView):
     # fields = '__all__'
     # fields = ('title', 'body')
 
+    def get_context_data(self, *args,**kwargs):
+        categories_menu = Category.objects.all()
+        context = super(self.__class__, self).get_context_data(*args, **kwargs)
+        context['categories_menu'] = categories_menu
+        return context
+
+
+class AddCategory(CreateView):
+    model = Category
+    template_name = 'addcategory.html'
+    fields = '__all__'
+
+    def get_context_data(self, *args,**kwargs):
+        categories_menu = Category.objects.all()
+        context = super(self.__class__, self).get_context_data(*args, **kwargs)
+        context['categories_menu'] = categories_menu
+        return context
+
 
 class EditPost(UpdateView):
     model = Post
@@ -39,37 +82,22 @@ class EditPost(UpdateView):
     template_name = 'editpost.html'
     # fields = ['title', 'title_tag', 'body']
 
+    def get_context_data(self, *args,**kwargs):
+        categories_menu = Category.objects.all()
+        context = super(self.__class__, self).get_context_data(*args, **kwargs)
+        context['categories_menu'] = categories_menu
+        return context
+
 
 class DeletePost(DeleteView):
     model = Post
     template_name = 'deletepost.html'
     success_url = reverse_lazy('home')
 
+    def get_context_data(self, *args,**kwargs):
+        categories_menu = Category.objects.all()
+        context = super(self.__class__, self).get_context_data(*args, **kwargs)
+        context['categories_menu'] = categories_menu
+        return context
 
 
-# def loginuser(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-
-#         user = authenticate(request, username=username, password=password)
-
-#         if user is not None:
-#             login(request, user)
-#             return redirect('posts')
-#     context = {}
-#     return render(request, 'login.html', context)
-
-# def registeruser(request):
-#     form = CreateUserForm()
-
-#     if request.method == 'POST':
-#         form = CreateUserForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             user = form.cleaned_data.get('username')
-#             messages.success(request, 'Account was created for ' + user)
-#             return redirect('login')
-
-#     context = {'form': form}
-#     return render(request, 'register.html', context)
--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserRegister
 
 urlpatterns = [
-    path('register/', UserRegister.as_view(), 'register'),
+    path('register/', UserRegister.as_view(), name='register'),
 ]
